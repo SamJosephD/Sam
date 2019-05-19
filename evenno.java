76e605fae9965f82d48d2aa8a2b4fc1f8155f9d6
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 22:33:26 +0530

    evenno.java

---
+++ evenno.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class evenno
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	long i,n,q;
+	n=get.nextLong();
+	q=get.nextLong();
+	
+	for(i=n+1;i<q;i++)
+	{
+	if(i%2==0)
+		{
+		System.out.print(i+" ");
+		}
+	}
+	}
+
+}
