ffa53dbecdfb4e00ad18ea1576c7621c274f3c52
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 22:45:16 +0530

    armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -1,29 +1,29 @@
 import java.io.*;
 import java.util.*;
 
-public class armstrongno
+public class armstrong
 {
 public static void main(String[] args)
 	{
-	Scanner get=new Scanner(System.in);	
-	long n,q,i,a;
-	n=get.nextLong();
-	q=get.nextLong();
-
-	for(i=n+1;i<q;i++)
-		{
-		long temp=i,k=0;
-		while(temp>0)
-			{
-			a=temp%10;
-			k=k+(a*a*a);
-			temp=temp/10;
-			}
-		if(k==i)
-		{
-		System.out.print(k+" ");
-		}
-		}
+	Scanner get=new Scanner(System.in);
+	int n,a,temp,k=0;
+	n=get.nextInt();
+	temp=n;
+	while(temp>0)
+	{
+	a=temp%10;
+	k=k+(a*a*a);
+	temp=temp/10;
+	}
+	if(k==n)
+	{
+	System.out.print("yes");	
+	}
+	else
+	{
+	System.out.print("no");	
+	}
+	
 	}
 
 }
