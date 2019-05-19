89733c0b0008276ba67e44dc6f7e67b91f611dd0
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 22:50:01 +0530

    multiple5.java

---
+++ multiple5.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class multiple5
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	long i,n,q=0;
+	n=get.nextLong();
+	for(i=1;i<=5;i++)
+	{
+	q=i*n;
+	System.out.print(q +" ");
+	}
+	
+	}
+
+}
