d4bafaff08afa75845f9d8f26f8df718011a3f89
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 22:35:25 +0530

    primeno.java

---
+++ primeno.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class primeno
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	long n,q,i,j,count;
+	n=get.nextLong();
+	q=get.nextLong();
+	String prime = "";
+
+	for(i=n;i<q;i++)
+	{
+	count=0;
+		for(j=i;j>=1;j--)
+		{
+		if(i%j==0)
+			{
+			count++;
+			}
+
+		
+		}
+		if(count==2)
+			{
+			prime = prime + i +" ";
+			}
+
+	}
+	System.out.print(prime);	
+	}
+
+}
