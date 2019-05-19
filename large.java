57ba7c1ffa456ddbdbeea1206fb8944f83bb446e
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 21:39:58 +0530

    large.java

---
+++ large.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class large
+{
+public static void main(String[] args)
+	{
+	
+	Scanner get=new Scanner(System.in);
+	long a,b,c;
+	a=get.nextLong();
+	b=get.nextLong();
+	c=get.nextLong();
+	if(a>c && a>b)
+		{
+		System.out.print(a);
+		}
+	else if(b>a && b>c)
+		{
+		System.out.print(b);
+		}
+	else if(c>a && c>b)
+		{
+		System.out.print(c);
+		}
+	}
+	
+
+
+}
