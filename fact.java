a74cb77c2f021e0c48bc0d17914eefc453a729f4
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 22:48:15 +0530

    fact.java

---
+++ fact.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class fact
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	long n,i,sum=1;
+	n=get.nextLong();
+	for(i=n;i>=1;i--)
+		{
+		sum=sum*i;
+		}
+
+	System.out.print(sum);
+	}
+
+}
