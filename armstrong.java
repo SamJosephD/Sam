aa9267d2528900ccd5e22e6bf6d62921319173cd
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 22:37:56 +0530

    armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class armstrongno
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);	
+	long n,q,i,a;
+	n=get.nextLong();
+	q=get.nextLong();
+
+	for(i=n+1;i<q;i++)
+		{
+		long temp=i,k=0;
+		while(temp>0)
+			{
+			a=temp%10;
+			k=k+(a*a*a);
+			temp=temp/10;
+			}
+		if(k==i)
+		{
+		System.out.print(k+" ");
+		}
+		}
+	}
+
+}
