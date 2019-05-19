92ffc9faacba555a440a898aacc89de437d3527d
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 21:45:51 +0530

    alpha.java

---
+++ alpha.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class alpha
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	char s;
+	s=get.next().charAt(0);
+	if((s>='a' && s<='z')||(s>='A' && s<='Z'))
+		{
+		System.out.print("Alphabet");
+		}
+	else
+		{
+		System.out.print("not");
+		}
+	}
+
+}
