60159210ecb68b33f07db617d6ad56fd5a92b4c7
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 21:35:01 +0530

    hello.java

---
+++ hello.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class hello
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	double i;
+	double a=get.nextDouble();
+	for(i=1;i<=a;i++)
+	{
+	System.out.println("Hello");
+	}	
+
+	}
+
+}
