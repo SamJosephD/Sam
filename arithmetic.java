80720109f35edbf8bdb30e0a2d23a663084cefaa
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 22:53:43 +0530

    arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class arithmetic
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	long n,a,d,temp,sum=0,i;
+	n=get.nextLong();
+	a=get.nextLong();
+	d=get.nextLong();
+	for(i=0;i<n;i++)
+	{
+	temp=a+(i*d);
+	sum=sum+temp;
+	
+	}
+	System.out.print(sum);
+
+	}
+
+}
