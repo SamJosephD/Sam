c1e5f771b6d1653a9aed8f8b7b906e34b7e7b25a
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 22:26:57 +0530

    palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class palindrome
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n=get.nextInt();
+	int a,sum=0;
+	int b=n;
+	while(b>0)
+	{
+	a=b%10;
+	
+	sum=(sum*10)+a;
+	
+	b=b/10;
+	}
+	if(sum==n)
+	{
+	System.out.print("yes");
+	}
+	else
+	{
+	System.out.print("no");
+	}
+	}
+
+}
