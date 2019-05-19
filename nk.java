a186e412fd91a72ddee9dc1da913a6062a9126be
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-19 21:29:35 +0530

    nk.java

---
+++ nk.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class nk
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n,k,i;
+	n=get.nextInt();
+	k=get.nextInt();
+	int size=n;
+	int[] arr=new int[n];
+	int sum=0;
+	for(i=0;i<n;i++)
+		{
+		arr[i]=get.nextInt();
+		}
+	for(i=0;i<k;i++)
+		{
+		sum=sum+arr[i];
+		}
+	
+	System.out.print(sum);
+	}
+
+}
