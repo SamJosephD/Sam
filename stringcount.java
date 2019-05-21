99b0a67d3b3727753b7404a9ff15982aece2e681
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-21 19:30:36 +0530

    Create stringcount.java

---
+++ stringcount.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class stringcount
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	String s="";
+	s=get.nextLine();
+	int n=get.nextInt(),i;
+	for(i=0;i<n;i++)
+	{
+	System.out.println(s);
+	}
+
+
+	}
+
+}
