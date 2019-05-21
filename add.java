9f431e38e3aaacc1acf406df65af2f22e172e71f
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-21 19:43:26 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class add
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int a,b;
+	a=get.nextInt();
+	b=get.nextInt();
+	a=a+b;
+	System.out.print(a);	
+	}
+
+}
