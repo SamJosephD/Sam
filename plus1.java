b036d17aca9007def873b9c497dc5d34c6921e6f
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-21 19:38:16 +0530

    Create plus1.java

---
+++ plus1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class plus1
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n=get.nextInt();
+	n=n+1;
+	System.out.print(n);	
+	}
+
+}
