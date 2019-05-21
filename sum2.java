71d7eb712dde9e64225db6647c55068fb36c3935
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-22 00:08:55 +0530

    Create sum2.java

---
+++ sum2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class sum2
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	long i,s,sum=0;
+	long a=get.nextLong();
+	for(s=0;s<=a;s++)
+	{
+	
+	
+	sum=sum+s;
+	}
+	System.out.print(sum);
+	}
+
+}
