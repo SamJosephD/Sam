e0c110fd690f708ee6083a260773d0ed63e25084
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-21 19:35:25 +0530

    Create oneor9.java

---
+++ oneor9.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class oneor9
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int s;
+	s=get.nextInt();
+	if(s==1||s==2||s==3||s==4||s==5||s==6||s==7||s==8||s==9||s==10)
+	{
+	System.out.print("yes");
+
+	}
+	else
+	{
+	System.out.print("no");
+
+	}
+	
+	}
+
+}
