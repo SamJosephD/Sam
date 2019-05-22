b0d527397f2153c3baf4f90abd5cb146db0f83f2
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-22 20:58:41 +0530

    Create words.java

---
+++ words.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class words
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int l,i,count=1;
+	String s;
+	s=get.nextLine();
+	l=s.length();
+	char[] ch=s.toCharArray();
+	for(i=0;i<l;i++)
+	{
+	if(ch[i]==' ')
+		{
+		count++;
+		}
+	}
+	System.out.print(count);
+	
+	}
+	
+
+	
+
+}
