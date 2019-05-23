b8300499789bd36fb03f0dee94c7faa07fa63453
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 15:30:50 +0530

    Create nobet.java

---
+++ nobet.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class nobet
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n,a,b;
+	n=get.nextInt();
+	a=get.nextInt();
+	b=get.nextInt();
+	if(n>a && n<b)
+	{
+	System.out.print("yes");
+	}
+	else
+	{
+	System.out.print("no");
+	}
+
+
+
+	
+	
+
+	}
+
+}
