d6a9d6c45e4f5e155d31f0074c755ada2056c0c0
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 22:57:35 +0530

    Create code93.java

---
+++ code93.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class code93
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n,m,sum;
+	n=get.nextInt();
+	m=273;
+	sum=n+m;
+	System.out.print(sum);
+	
+	
+	
+	}
+
+}
