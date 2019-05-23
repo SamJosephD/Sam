242b2e38f065d044980721a551db0523b28b8d3c
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 15:01:49 +0530

    Create minof10.java

---
+++ minof10.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class minof10
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n,l,i;
+	n=10;
+	int[] arr =new int[n];
+	for(i=0;i<n;i++)
+	{
+	arr[i]=get.nextInt();
+	}
+	l=arr[0];
+	for(i=0;i<n;i++)
+		{
+		if(l>arr[i])
+			{
+			l=arr[i];
+			}
+		}
+	System.out.print(l);
+
+	}
+
+}
