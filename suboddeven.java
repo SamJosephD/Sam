1ba360294514e213686d9840317a99ba48d57c9c
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 15:08:58 +0530

    Create suboddeven.java

---
+++ suboddeven.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class suboddeven
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n,m,p,q;
+	n=get.nextInt();
+	m=get.nextInt();
+	p=m-n;
+	q=p%2;
+	if(q==0)
+	{
+	System.out.print("even");
+	}
+	else if(q==1)
+	{
+	System.out.print("odd");
+	}
+	
+
+	}
+
+}
