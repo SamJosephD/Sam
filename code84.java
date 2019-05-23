9293fdac6316ef4636d4f9a423887e97433ef05c
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 22:29:58 +0530

    Create code84.java

---
+++ code84.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class 
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	char c=get.next().charAt(0);
+	int n= c;
+	System.out.print(n);
+	
+
+	}
+
+}
