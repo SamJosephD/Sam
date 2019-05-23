be30fcb8e6b89cf5834cb7e73102b49ccc176924
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 22:26:45 +0530

    Create code82.java

---
+++ code82.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class code82
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	float n,m;
+	n=get.nextFloat();
+	m=get.nextFloat();
+	float c=m*n;
+	System.out.printf("%.5f",c);
+
+
+	}
+
+}
