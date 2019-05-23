481fb1e47c1a66aa47d326a0a5e9bad82a465f8e
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 22:23:44 +0530

    Create code81.java

---
+++ code81.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class code81
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n,m;
+	n=get.nextInt();
+	m=get.nextInt();	
+	int c=m-n;
+	System.out.print(c);
+	}
+
+}
