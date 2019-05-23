a062188e68d65fe36553e76767bd6a61d82effd4
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 15:07:51 +0530

    Create modof7.java

---
+++ modof7.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class modof7
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n;
+	n=get.nextInt();
+	if(n%7==0)
+	{
+	System.out.print("yes");
+	}
+	else
+	{
+	System.out.print("no");
+	}
+	
+
+	}
+
+}
