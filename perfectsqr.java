3fe28d8131a61b163310e41a920860dc4775f7be
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 15:37:16 +0530

    Create perfectsqr.java

---
+++ perfectsqr.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class perfectsqr
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	int n,m;
+	n=get.nextInt();
+	m=get.nextInt();
+	
+	if(n==m)
+	{
+	System.out.print("yes");
+	}
+	else
+	{
+	System.out.print("no");
+	}
+	
+
+	}
+
+}
