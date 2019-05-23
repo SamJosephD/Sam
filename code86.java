aab898eadd0093d038be544487b47b58b9020305
SamJosephD <50749024+SamJosephD@users.noreply.github.com>
2019-05-23 22:37:30 +0530

    Create code86.java

---
+++ code86.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class code86
+{
+public static void main(String[] args)
+	{
+	Scanner get=new Scanner(System.in);
+	String s=get.nextLine();
+	char[] ch=s.toCharArray();
+	int l=s.length(),i,j,count=0;
+	for(i=0;i<l;i++)
+		{
+		for(j=0;j<l;j++)
+			if(ch[i]==ch[j])
+			{
+			count++;
+			}
+	
+	
+		}
+	
+	if(count==l)
+	{
+	System.out.print("yes");
+	}
+	else
+	System.out.print("no");
+	
+	}
+
+}
